0f1cddf3f48f8f7bb88e22745ed0753b49d9e28b
sanjaypj <sanjaypjayan2000@gmail.com>
2020-01-27 00:20:44 +0530

    home page completed

---
+++ index.php
@@ -0,0 +1,241 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/inter/inter.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css">
+    <title>Join the band - Spotify Jobs</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3">
+        <div class="container">
+            <a class="navbar-brand w-800" href="#">Spotify Jobs</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav w-600 ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Job Categories <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Locations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Go Backstage</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Students</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Search Jobs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container my-5 py-5 text-center">
+        <div class="my-5 py-5">
+            <h1 class="w-800 display-2 text-center">Join the band</h1>
+            <h5 class="w-600 text-muted text-center" style="line-height: 30px">Lorem ipsum dolor sit amet, consectetur adipisicing <br> elit. Recusandae consequuntur, vel sequi.</h5>
+            <button class="btn btn-primary btn-lg w-600 px-5 badge-pill mt-3 text-uppercase">Go Now</button>
+        </div>
+    </div>
+    <div class="bg-light py-5">
+        <div class="container py-5 my-5">
+            <h1 class="w-800 text-center mb-5" style="font-size: 50px">What's your passion?</h1>
+            <div class="row">
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Anchor</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Brand & Creative</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Content</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Customer Service Experience</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Data Analytics</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Design and User Expericence</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Engineering</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+                <div class="col-3 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Finance</h3>
+                    <h6 class="text-primary">67 Positions -></h6>
+                </div>
+            </div>
+            <div class="text-center mt-5">
+                <button class="btn btn-primary btn-lg w-600 px-5 badge-pill mt-5 text-uppercase">All categories</button>
+            </div>
+        </div>
+    </div>
+    <div class="bg-dark py-5">
+        <div class="container pt-5">
+            <div style="width: 600px" class="my-5 py-5">
+                <h2 class="w-700 text-white mt-5 pt-4" style="line-height: 45px">The most rewarding thing is when you see people grow. To see them taking on more responsibility, setting their own goals and realizing what’s important to them. This happens at Spotify, every day.</h2>
+                <h5 class="text-white mt-4">Daniel Ek, CEO</h5>
+            </div>
+        </div>
+    </div>
+    <div class="bg-light py-5">
+        <div class="container py-5 my-5">
+            <div class="bg-primary my-5" style="height: 400px; width: 400px">
+            </div>
+        </div>
+    </div>
+    <div class="py-5">
+        <div class="container py-5 my-5">
+            <h1 class="w-800 text-center mb-5" style="font-size: 50px">Featured Jobs</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Job</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <h4 class="w-700">Sound Design Specialist – Parcast</h4>
+                        </td>
+                        <td class="text-primary">Otto</td>
+                        <td class="text-primary">@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4 class="w-700">C++ Native Developer – Home Platform</h4>
+                        </td>
+                        <td class="text-primary">Otto</td>
+                        <td class="text-primary">@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4 class="w-700">Product Manager – Machine Learning, Feature Storage</h4>
+                        </td>
+                        <td class="text-primary">Otto</td>
+                        <td class="text-primary">@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4 class="w-700">Operations Manager, SoundBetter</h4>
+                        </td>
+                        <td class="text-primary">Otto</td>
+                        <td class="text-primary">@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <h4 class="w-700">Sound Design Specialist – Parcast</h4>
+                        </td>
+                        <td class="text-primary">Otto</td>
+                        <td class="text-primary">@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="text-center mt-5">
+                <button class="btn btn-primary btn-lg w-600 px-5 badge-pill mt-5 text-uppercase">View all jobs</button>
+            </div>
+        </div>
+    </div>
+    <div class="bg-light py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-4 px-5 my-5 py-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Anchor</h3>
+                    <p class="text-muted w-500">Lorem ipsum dolor sit?</p>
+                    <h6 class="text-primary text-uppercase mt-3">Read more</h6>
+                </div>
+                <div class="col-4 px-5 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Brand & Creative</h3>
+                    <p class="text-muted w-500">Lorem ipsum dolor sit?</p>
+                    <h6 class="text-primary text-uppercase mt-3">Read more</h6>
+                </div>
+                <div class="col-4 px-5 mt-5 pt-3 text-center">
+                    <img src="https://dummyimage.com/640x640/fff/aaa" class="w-100 rounded-circle" alt="">
+                    <h3 class="w-700 mt-4">Content</h3>
+                    <p class="text-muted w-500">Lorem ipsum dolor sit?</p>
+                    <h6 class="text-primary text-uppercase mt-3">Read more</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="py-5 bg-dark">
+        <div class="container py-5">
+            <div class="row text-white">
+                <div class="col-12 col-md">
+                    <h3 class="w-600">Spotify Jobs</h3>
+                    <small class="d-block mb-3 text-light">© 2017-2019</small>
+                </div>
+                <div class="col-6 col-md">
+                    <h5 class="mb-4 w-700">Features</h5>
+                    <ul class="list-unstyled text-small">
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Cool stuff</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Random feature</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Team feature</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Stuff for developers</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Another one</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Last time</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5 class="mb-4 w-700">Resources</h5>
+                    <ul class="list-unstyled text-small">
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Resource</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Resource name</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Another resource</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Final resource</a></li>
+                    </ul>
+                </div>
+                <div class="col-6 col-md">
+                    <h5 class="mb-4 w-700">About</h5>
+                    <ul class="list-unstyled text-small">
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Team</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Locations</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Privacy</a></li>
+                        <li class="mb-3"><a class="text-muted w-600" href="#">Terms</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
